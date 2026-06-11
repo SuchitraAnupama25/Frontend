f3049c3610da26c02d1ffd710bfa096bba1845fd
SuchitraAnupama25 <anupamayadavalli2511@gmail.com>
2026-06-11 08:58:17 +0530

    initial commit

---
+++ JavaScript/learn_extracting_data.js
@@ -0,0 +1,17 @@
+//without destructuring
+
+const colors = ["Red","Green","Blue"];
+const first = colors[0];
+const second = colors[1];
+const third = colors[2];
+console.log(first);
+console.log(second);
+console.log(third);
+
+//with destructuring
+
+const colours = ["Red","Green","Blue"];
+const[first1,second2,third3] = colours;
+console.log(first1);
+console.log(second2);
+console.log(third3);
