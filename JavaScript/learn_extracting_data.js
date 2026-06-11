983a1117e741b68b64330ac2b87257472f9e9664
SuchitraAnupama25 <anupamayadavalli2511@gmail.com>
2026-06-11 09:39:29 +0530

    initial commit

--- JavaScript/learn_extracting_data.js
+++ JavaScript/learn_extracting_data.js
@@ -49,3 +49,30 @@ let arr1 = [1,2,3,4];
 let arr2 = [5,6,7,8];
 let result = arr1.concat(arr2);
 console.log(result);
+
+//Spread operator with array
+const num1 = [1,2,3];
+const num2 = [4,5,6];
+
+const result1 = [...num1,...num2];//spread operator (combines 2 array or object)
+const result2 = [...num1,40,50,...num2];
+console.log(result1);
+console.log(result2);
+
+const fruits = ["Apple","Mango","Orange"];
+const copiedFruits = [...fruits];
+console.log(copiedFruits);
+
+//spread operator with objects
+
+const employee2 = {
+    name:"Anupama",
+    role:"System Engineer"
+};
+
+const copiedEmployee2 = {
+    ...employee2,
+    ...{age:20}
+};
+
+console.log(copiedEmployee2);
